9d8ad8d648a7f9ad9d039ed17e2edf9b64e7db35
ashwani-luhaniwal <ashwani.kumar.luhaniwal@sap.com>
2017-12-17 23:12:44 +0530

    Initial setup for react app

---
+++ App.jsx
@@ -0,0 +1,13 @@
+// first react component
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello World!
+            </div>
+        );
+    }
+}
+export default App; // export the component when you want use it in other parts of app
---
+++ main.js
@@ -0,0 +1,6 @@
+// import App component and render it to our root App element
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+ReactDOM.render(<App />, document.getElementById('app'));
